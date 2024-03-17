9061af051f620102137d30d939289aceca6212a1
chriskar <christian@karwath24.de>
2024-03-17 15:45:36 +0100

    made volume decrease over distance

--- src/main/java/dev/chriskar/ghrange/mixin/GoatHornItemMixin.java
+++ src/main/java/dev/chriskar/ghrange/mixin/GoatHornItemMixin.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.GoatHornItem;
 import net.minecraft.item.Instrument;
-import net.minecraft.item.Instruments;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.Vec3d;
@@ -25,14 +24,16 @@ import java.util.Objects;
 @Mixin(GoatHornItem.class)
 public class GoatHornItemMixin {
     @Unique
-    private static final GameRules.Key<GameRules.IntRule> GOAT_HORN_RANGE = GameRuleRegistry.register("goatHornRange", GameRules.Category.MISC, GameRuleFactory.createIntRule(Instruments.GOAT_HORN_RANGE));
+    private static final int GOAT_HORN_RANGE_DEFAULT = 256;
+    @Unique
+    private static final GameRules.Key<GameRules.IntRule> GOAT_HORN_RANGE = GameRuleRegistry.register("goatHornRange", GameRules.Category.MISC, GameRuleFactory.createIntRule(GOAT_HORN_RANGE_DEFAULT));
     @Unique
     private static final Random random = Random.create();
 
     @Redirect(method = "use",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/item/GoatHornItem;playSound(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/Instrument;)V"))
     private void playSound(World world, PlayerEntity player, Instrument instrument) {
-        float volume = world.getGameRules().getInt(GOAT_HORN_RANGE) / 16.0f;
+        float volume = GOAT_HORN_RANGE_DEFAULT / 16.0f;
 
         try {
             PlayerLookup.all(Objects.requireNonNull(world.getServer())).stream()
@@ -41,8 +42,11 @@ public class GoatHornItemMixin {
                     .forEach(p -> {
                         Vec3d directionToPlayer = player.getPos().subtract(p.getPos()).normalize().multiply(50);
                         Vec3d soundPosition = new Vec3d(p.getX() + directionToPlayer.x, player.getY(), p.getZ() + directionToPlayer.z);
+                        double distance = p.getPos().distanceTo(player.getPos());
+                        double percent = 1 - (distance / world.getGameRules().getInt(GOAT_HORN_RANGE));
+                        double playerVolume = volume * (-0.8 * percent * percent + 1.6 * percent + 0.2);
 
-                        p.networkHandler.sendPacket(new PlaySoundS2CPacket(instrument.soundEvent(), SoundCategory.RECORDS, soundPosition.getX(), soundPosition.getY(), soundPosition.getZ(), volume, 1, random.nextLong()));
+                        p.networkHandler.sendPacket(new PlaySoundS2CPacket(instrument.soundEvent(), SoundCategory.RECORDS, soundPosition.getX(), soundPosition.getY(), soundPosition.getZ(), (float) playerVolume, 1, random.nextLong()));
                     });
         } catch (NullPointerException ignored) {
             world.playSoundFromEntity(player, player, instrument.soundEvent().value(), SoundCategory.RECORDS, volume, 1.0f);
